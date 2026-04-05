64e4e627b3fb4634dd0b48cd6687fc6d237efd04
anurag <anuragkumargupta02@gmail.com>
2026-04-05 14:59:31 +0530

    config server demo

---
+++ springextconfig_p1/src/main/java/com/config/springextconfig_p1/SpringConfig.java
@@ -0,0 +1,30 @@
+package com.config.springextconfig_p1;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SpringConfig {
+    private final ConfigPojo pojo;
+
+    @Value("${framworkdetails.java_version}")
+     String java_version;
+    @Value("${framworkdetails.springbootversion:987}")
+     String springbootversion;
+
+    @GetMapping("/details")
+    public String getConfigDetails(){
+    return "build version " + pojo.getVersion()  + " build number :" + pojo.getBuidNum() + " package  :" + pojo.getPackagedetails();
+    }
+
+    @GetMapping("/framworkdetails")
+    public String getframworkDetails(){
+        return "java version " + java_version  + " string boot version :" +springbootversion ;
+    }
+
+}
