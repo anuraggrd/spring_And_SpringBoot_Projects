2852f4da7698d9c06c442b6cec6cbddc11eb5462
anurag <anuragkumargupta02@gmail.com>
2026-04-28 04:11:26 +0530

    commiting the api gateway configuration

---
+++ ecom_microservice/gateway/src/main/java/com/ecom/gateway/GatewayConfig.java
@@ -0,0 +1,46 @@
+package com.ecom.gateway;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("product-service", r -> r
+                        .path("/api/product/**")
+                        .filters(f -> f.retry(retryConfig -> retryConfig
+                                        .setRetries(10)
+                                        .setMethods(HttpMethod.GET)
+                                )
+                               )
+//                        .filters(f -> f.rewritePath("/products(?<segment>/?.*)",
+//                                "/api/products${segment}"))
+                        .uri("lb://PRODUCT-SERVICE"))
+                .route("user-service", r -> r
+                        .path("/user/api/**")
+//                        .filters(f -> f.rewritePath("/users(?<segment>/?.*)",
+//                                "/api/users${segment}"))
+                        .uri("lb://USER-SERVICE"))
+                .route("order-service", r -> r
+                        .path("/api/order/**", "/api/cart/**")
+//                        .filters(f -> f.rewritePath("/(?<segment>.*)",
+//                                "/api/${segment}"))
+                        .uri("lb://ORDER-SERVICE"))
+                .route("eureka-server", r -> r
+                        .path("/eureka/main")
+                        .filters(f -> f.rewritePath("/eureka/main", "/"))
+                        .uri("http://localhost:8761"))
+                .route("eureka-server-static", r -> r
+                        .path("/eureka/**")
+                        .uri("http://localhost:8761"))
+                .build();
+    }
+}
